6ac897a4d473266abd94f08af2af326519652bd3
eimwe <prokopenkoserge@inbox.ru>
2023-06-01 15:21:48 +0600

    Added wrappers to the pages

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,5 @@
 <script setup lang="ts"></script>
 
-<template>Home</template>
+<template>
+  <div class="mx-auto max-w-5xl px-8 py-6 lg:px-0">Home</div>
+</template>
--- src/views/StatsView.vue
+++ src/views/StatsView.vue
@@ -1,1 +1,5 @@
-<template>Stats</template>
+<script setup lang="ts"></script>
+
+<template>
+  <div class="mx-auto max-w-5xl px-8 py-6 lg:px-0">Stats</div>
+</template>
