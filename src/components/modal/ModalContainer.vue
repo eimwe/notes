881606f222cb49fba2157a8b6310fbc446ab8325
eimwe <prokopenkoserge@inbox.ru>
2023-06-02 15:35:56 +0600

    Created reusable modal container
    1. Destructurized close method and reactive node reference from useModal composable to make modal container work;
    2. Provided fallback slot content.

---
+++ src/components/modal/ModalContainer.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import CloseIcon from './icons/CloseIcon.vue'
+import useModal from '../../composables/useModal'
+
+const { modalRef, closeModal } = useModal()
+</script>
+
+<template>
+  <dialog ref="modalRef" class="modal-box bg-neutral-content text-neutral">
+    <button
+      @click="closeModal"
+      class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2 focus:outline-neutral"
+    >
+      <CloseIcon />
+    </button>
+    <slot>Press Esc or 'close' button to close modal</slot>
+  </dialog>
+</template>
