4f185e3565d1d60512906533217fb98dc242dfea
eimwe <prokopenkoserge@inbox.ru>
2023-06-02 12:31:00 +0600

    Marked up, styled and imported footer

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import NavBar from './components/layout/NavBar.vue'
+import FooterButton from './components/layout/FooterButton.vue'
 </script>
 
 <template>
   <NavBar />
   <RouterView />
+  <FooterButton />
 </template>
---
+++ src/components/layout/FooterButton.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import AddNoteIcon from './icons/AddNoteIcon.vue'
+</script>
+
+<template>
+  <footer class="sticky top-[100vh]">
+    <div class="flex justify-end mx-auto max-w-5xl px-4 py-6 sm:px-8 lg:px-0">
+      <button class="btn btn-lg btn-circle btn-accent mx-2 focus:outline-neutral">
+        <AddNoteIcon class="h-8 w-8" />
+        <span class="sr-only">Add new note</span>
+      </button>
+    </div>
+  </footer>
+</template>
