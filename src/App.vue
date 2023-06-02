5b3b43a9ac571bec18779c25f6941e7a86182e90
eimwe <prokopenkoserge@inbox.ru>
2023-06-02 16:29:41 +0600

    Rendered modal content dynamically
    Depended on the emitted event from children.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
+import { markRaw } from 'vue'
 import { RouterView } from 'vue-router'
 import NavBar from './components/layout/NavBar.vue'
 import FooterButton from './components/layout/FooterButton.vue'
 import ModalContainer from './components/modal/ModalContainer.vue'
+import AddNote from './components/modal/components/AddNote.vue'
+
+const modalContent = markRaw(AddNote)
 </script>
 
 <template>
@@ -10,6 +14,8 @@ import ModalContainer from './components/modal/ModalContainer.vue'
   <main>
     <RouterView />
   </main>
-  <FooterButton />
-  <ModalContainer />
+  <FooterButton @addNote="modalContent = AddNote" />
+  <ModalContainer>
+    <component :is="modalContent" />
+  </ModalContainer>
 </template>
