b61e04233e14ec835971d8605b0bba376c38e646
eimwe <prokopenkoserge@inbox.ru>
2023-06-02 16:28:07 +0600

    Modified add note method
    Emitted 'addNote' event along with showModal to differentiate it from other events in the future.

--- src/components/layout/FooterButton.vue
+++ src/components/layout/FooterButton.vue
@@ -3,15 +3,19 @@ import AddNoteIcon from './icons/AddNoteIcon.vue'
 import useModal from '../../composables/useModal'
 
 const { openModal } = useModal()
+
+const emit = defineEmits(['addNote'])
+
+const addNote = () => {
+  emit('addNote')
+  openModal()
+}
 </script>
 
 <template>
   <footer class="sticky top-[100vh]">
     <div class="flex justify-end mx-auto max-w-5xl px-4 py-6 sm:px-8 lg:px-0">
-      <button
-        @click="openModal"
-        class="btn btn-lg btn-circle btn-accent mx-2 focus:outline-neutral"
-      >
+      <button @click="addNote" class="btn btn-lg btn-circle btn-accent mx-2 focus:outline-neutral">
         <AddNoteIcon class="h-8 w-8" />
         <span class="sr-only">Add new note</span>
       </button>
