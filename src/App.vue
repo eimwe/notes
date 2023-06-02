4dd3c055cfe6573d3368f0616a93d57c9274cd15
eimwe <prokopenkoserge@inbox.ru>
2023-06-02 15:39:01 +0600

    Imported modal container to the App level
    Wrapped router-view inside main tag for semantic reasons.

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,14 @@
 import { RouterView } from 'vue-router'
 import NavBar from './components/layout/NavBar.vue'
 import FooterButton from './components/layout/FooterButton.vue'
+import ModalContainer from './components/modal/ModalContainer.vue'
 </script>
 
 <template>
   <NavBar />
-  <RouterView />
+  <main>
+    <RouterView />
+  </main>
   <FooterButton />
+  <ModalContainer />
 </template>
