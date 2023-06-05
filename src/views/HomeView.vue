1bbb5826255ee3ec9edb39b82513b5586c65bace
eimwe <prokopenkoserge@inbox.ru>
2023-06-05 16:01:27 +0600

    Updated home page with note sample
    1. Imported card note component with the default content;
    2. Fixed inline padding mq breakpoint for the wrapper.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import NoteCard from '../components/card/NoteCard.vue'
+</script>
 
 <template>
-  <div class="mx-auto max-w-5xl px-8 py-6 lg:px-0">Home</div>
+  <div
+    class="grid gap-2 grid-flow-row sm:grid-cols-2 lg:grid-cols-3 mx-auto max-w-5xl px-8 py-6 xl:px-0"
+  >
+    <NoteCard />
+  </div>
 </template>
