2c7bfc3e2b86a62b6dfac1e1ac94cc9e978c9de8
eimwe <prokopenkoserge@inbox.ru>
2023-06-07 13:28:23 +0600

    Linked add note button with store data and action
    1. Imported and initiated store in AddNote component;
    2. Linked textarea input with store data via v-model (added 'trim' modifier as well);
    3. Applied addNote handler on form submit.

--- src/components/modal/components/AddNote.vue
+++ src/components/modal/components/AddNote.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
+import { useNoteStore } from '../../../stores/notes'
 import ButtonSlot from '../../button/ButtonSlot.vue'
+
+const store = useNoteStore()
 </script>
 
 <template>
-  <form method="dialog">
+  <form method="dialog" @submit="store.addNote">
     <h3 class="font-bold text-lg mb-6">Add new note</h3>
     <div class="grid gap-4">
       <textarea
+        v-model.trim="store.note"
         placeholder="What's on your mind?"
         class="textarea textarea-bordered textarea-ghost focus:textarea-ghost textarea-primary textarea-lg w-full placeholder:text-slate-500"
       ></textarea>
