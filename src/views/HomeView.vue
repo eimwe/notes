d6013f9dc1aee430ac124617c6565ee6e184afa1
eimwe <prokopenkoserge@inbox.ru>
2023-06-07 13:36:23 +0600

    Rendered notes/stub on the home page
    1. Imported and initiated store;
    2. Replaced wrapper with 2 sections:
    	- One section is rendered when there are no notes to display;
    	- Another section is rendered otherwise.
    3. Looped through store data to render all notes.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,24 @@
 <script setup lang="ts">
+import { useNoteStore } from '../stores/notes'
 import NoteCard from '../components/card/NoteCard.vue'
+
+const store = useNoteStore()
 </script>
 
 <template>
-  <div
+  <section
+    v-if="!store.notes.length"
+    class="grid min-h-[70vh] items-center text-center mx-auto max-w-5xl px-8 xl:px-0"
+  >
+    <h2 class="text-xl sm:text-2xl">There are no notes to display.<br />Add some notes</h2>
+  </section>
+  <section
+    v-else
     class="grid gap-2 grid-flow-row sm:grid-cols-2 lg:grid-cols-3 mx-auto max-w-5xl px-8 py-6 xl:px-0"
   >
-    <NoteCard />
-  </div>
+    <h2 class="sr-only">Here are all your notes</h2>
+    <NoteCard v-for="note in store.notes" :key="note.id">
+      {{ note.memo }}
+    </NoteCard>
+  </section>
 </template>
