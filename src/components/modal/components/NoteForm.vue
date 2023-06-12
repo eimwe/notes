81e606024ada324734ab08998ca1322a0d6fb1f5
eimwe <prokopenkoserge@inbox.ru>
2023-06-12 20:07:52 +0600

    Created new NoteForm component for modal
    It combines both adding and editing tasks. It receives the following props:
    1. 'title' - defines the task (Add note or edit note);
    2. 'id' (optional) - defines the handler. If there is an ID, the form presents editing feature. Otherwise - new note adding.

---
+++ src/components/modal/components/NoteForm.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { useNoteStore } from '../../../stores/notes'
+import ButtonSlot from '../../button/ButtonSlot.vue'
+import type { ID } from '@/types'
+
+const store = useNoteStore()
+
+const props = defineProps<{
+  title: string
+  id?: ID
+}>()
+</script>
+
+<template>
+  <form method="dialog" @submit="store.changeNotes(id)">
+    <h3 class="font-bold text-lg mb-6">
+      {{ props.title }}
+    </h3>
+    <div class="grid gap-4">
+      <textarea
+        v-model.trim="store.note"
+        placeholder="What's on your mind?"
+        class="textarea textarea-bordered textarea-ghost focus:textarea-ghost textarea-primary textarea-lg w-full placeholder:text-slate-500"
+      ></textarea>
+      <ButtonSlot class="btn-primary sm:btn-sm sm:justify-self-end"> Save </ButtonSlot>
+    </div>
+  </form>
+</template>
