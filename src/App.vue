4d5664e663f012e52aaf26447aca69dbc35ae3a8
eimwe <prokopenkoserge@inbox.ru>
2023-06-12 20:30:00 +0600

    Overhauled the App
    1. Deleted previous imports for the dynamic modal content and related logic as well. Imported one 'NoteForm' instead;
    2. Imported 'ModalParams' interface and created the object of the same name to handle switch between editing/adding notes;
    3. Added 'onAdd'/'onEdit' methods and defined parameters (modal title + optional note ID) that were passed to the 'NoteForm' as props;
    4. Reformed 'RouterView' into dynamic component to be able to listen to the 'edit-note' event emitted by the homepage;
    5. Replaced modal insides with 'NoteForm' component and passed modal title + optional note ID as props.

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,37 @@
 <script setup lang="ts">
-import { markRaw } from 'vue'
+import { reactive } from 'vue'
 import { RouterView } from 'vue-router'
 import NavBar from './components/layout/NavBar.vue'
 import FooterButton from './components/layout/FooterButton.vue'
 import ModalContainer from './components/modal/ModalContainer.vue'
-import AddNote from './components/modal/components/AddNote.vue'
+import NoteForm from './components/modal/components/NoteForm.vue'
+import type { ModalParams, ID } from '@/types'
 
-const modalContent = markRaw(AddNote)
+const modalParams: ModalParams = reactive({
+  title: 'Change note',
+  id: undefined
+})
+
+const onAdd = () => {
+  modalParams.title = 'Add note'
+  modalParams.id = undefined
+}
+
+const onEdit = (noteId: ID) => {
+  modalParams.title = 'Edit note'
+  modalParams.id = noteId
+}
 </script>
 
 <template>
   <NavBar />
   <main>
-    <RouterView />
+    <RouterView v-slot="{ Component }">
+      <component :is="Component" @edit-note="onEdit" />
+    </RouterView>
   </main>
-  <FooterButton @addNote="modalContent = AddNote" />
+  <FooterButton @add-note="onAdd" />
   <ModalContainer>
-    <component :is="modalContent" />
+    <NoteForm :title="modalParams.title" :id="modalParams.id" />
   </ModalContainer>
 </template>
