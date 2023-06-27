22cb03dd7df0e8e9770aedcf146653514057d287
eimwe <prokopenkoserge@inbox.ru>
2023-06-27 12:57:56 +0600

    Limited note character length
    1. Added 'maxlength' attribute to the textarea and bound it to the constant of the same name defined in the <script setup>;
    2. Rendered warning message in case of user's input exceeds the limit.

--- src/components/modal/components/NoteForm.vue
+++ src/components/modal/components/NoteForm.vue
@@ -2,10 +2,12 @@
 import { useNoteStore } from '../../../stores/notes'
 import useModal from '../../../composables/useModal'
 import ButtonSlot from '../../button/ButtonSlot.vue'
+import WarningIcon from './icons/WarningIcon.vue'
 import type { ID } from '@/types'
 
 const store = useNoteStore()
 const { closeModal } = useModal()
+const maxlength = 140
 
 const props = defineProps<{
   title: string
@@ -30,9 +32,17 @@ const submitForm = () => {
         name="text-note"
         placeholder="What's on your mind?"
         class="textarea textarea-bordered textarea-ghost focus:textarea-ghost textarea-primary textarea-lg w-full placeholder:text-slate-500"
+        :maxlength="maxlength"
         autofocus
         required
       ></textarea>
+      <div
+        v-show="store.note.length === maxlength"
+        class="alert alert-warning flex-row justify-normal p-[0.5rem] my-0"
+      >
+        <WarningIcon />
+        <span class="!m-0">Your note is too lengthy</span>
+      </div>
       <ButtonSlot class="btn-primary sm:btn-sm sm:justify-self-end" :disabled="!store.note">
         Save
       </ButtonSlot>
