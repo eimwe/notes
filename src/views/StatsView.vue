5f86425a2412ac7759b4e8a62a5eaa92577f4029
eimwe <prokopenkoserge@inbox.ru>
2023-06-28 12:01:52 +0600

    Updated Stats page with computed data from the store
    1. Replaced all hardcoded data with computed results coming from the store's getters and/or constants;
    2. Added a description for 'Character limit' column indicating that provided limitation applies to each note.

--- src/views/StatsView.vue
+++ src/views/StatsView.vue
@@ -1,22 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useNoteStore } from '@/stores/notes'
+
+const store = useNoteStore()
+</script>
 
 <template>
   <div class="mx-auto max-w-5xl px-4 py-6 text-center sm:text-left sm:px-8 xl:px-0">
     <div class="stats stats-vertical sm:stats-horizontal shadow">
       <div class="stat">
         <div class="stat-title capitalize">Total notes</div>
-        <div class="stat-value">31K</div>
+        <div class="stat-value">{{ store.countNotes }}</div>
       </div>
 
       <div class="stat">
         <div class="stat-title capitalize">Character counter</div>
-        <div class="stat-value">4,200</div>
+        <div class="stat-value">{{ store.countCharacters }}</div>
         <div class="stat-desc">across all notes</div>
       </div>
 
       <div class="stat">
         <div class="stat-title capitalize">Character limit</div>
-        <div class="stat-value">200</div>
+        <div class="stat-value">{{ store.characterLimit }}</div>
+        <div class="stat-desc">per 1 note</div>
       </div>
     </div>
   </div>
