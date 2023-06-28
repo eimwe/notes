df2c39de125da4884b4be743358b3ac4719d94c3
eimwe <prokopenkoserge@inbox.ru>
2023-06-28 11:03:13 +0600

    Populated Stats page with hardcoded data

--- src/views/StatsView.vue
+++ src/views/StatsView.vue
@@ -1,5 +1,23 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="mx-auto max-w-5xl px-8 py-6 lg:px-0">Stats</div>
+  <div class="mx-auto max-w-5xl px-4 py-6 text-center sm:text-left sm:px-8 xl:px-0">
+    <div class="stats stats-vertical sm:stats-horizontal shadow">
+      <div class="stat">
+        <div class="stat-title capitalize">Total notes</div>
+        <div class="stat-value">31K</div>
+      </div>
+
+      <div class="stat">
+        <div class="stat-title capitalize">Character counter</div>
+        <div class="stat-value">4,200</div>
+        <div class="stat-desc">across all notes</div>
+      </div>
+
+      <div class="stat">
+        <div class="stat-title capitalize">Character limit</div>
+        <div class="stat-value">200</div>
+      </div>
+    </div>
+  </div>
 </template>
