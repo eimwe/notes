5f62d5df3c3bde81406554d56397425ef2ce6640
eimwe <prokopenkoserge@inbox.ru>
2023-06-30 12:48:55 +0600

    Configured PWA Vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,54 @@
 import { fileURLToPath, URL } from 'node:url'
+import { VitePWA } from 'vite-plugin-pwa'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      includeAssets: [
+        'favicon.ico',
+        'apple-touch-icon.png',
+        'android-chrome-192x192.png',
+        'android-chrome-512x512.png'
+      ],
+      manifest: {
+        name: 'Notes Progressive Web App',
+        short_name: 'Notes',
+        description: 'Installable app to create notes',
+        theme_color: '#333c4d',
+        background_color: '#0f172a',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
+  ],
   base: '/notes/',
   resolve: {
     alias: {
